87f2208bf4eab4689263de275738011801018bf3
luciarevaliente <1668160@uab.cat>
2026-06-28 15:20:39 +0200

    Cleaning repo

--- infrastructure/terraform/modules/biomedical_pipeline/kinesis.tf
+++ infrastructure/terraform/modules/biomedical_pipeline/kinesis.tf
@@ -1,11 +1,11 @@
 # Kinesis Data Stream for biomedical data ingestion
 resource "aws_kinesis_stream" "biomedical_stream" {
   name             = "${var.project_name}-${var.environment}-stream"
-  shard_count      = 5 #1 --> 2 --> 5
+  # shard_count      = 5 #1 --> 2 --> 5
   retention_period = 24
 
   stream_mode_details {
-    stream_mode = "PROVISIONED"
+    stream_mode = "ON_DEMAND"
   }
 
   tags = {
